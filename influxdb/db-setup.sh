cca418cf90c5649cb1bb250b64f398d09dbe2131
M.Gossman <mark.gossman71@gmail.com>
2025-10-31 06:13:14 -0500

    Update InfluxDB setup script with new password

--- influxdb/db-setup.sh
+++ influxdb/db-setup.sh
@@ -1,17 +1,17 @@
 # Create DB "proxmox"
-docker exec -it influxdb influx -username admin -password 'CHANGE_ME' \
+docker exec -it influxdb influx -username admin -password 'secret' \
   -execute 'CREATE DATABASE "proxmox"'
 
 # (optional) keep only 30 days by default
-#docker exec -it influxdb influx -username admin -password 'CHANGE_ME' \
+#docker exec -it influxdb influx -username admin -password 'secret' \
   -execute 'CREATE RETENTION POLICY "thirty_days" ON "proxmox" DURATION 30d REPLICATION 1 DEFAULT'
 
 
 # create a Proxmox-specific user
-docker exec -it influxdb influx -username admin -password 'CHANGE_ME' \
+docker exec -it influxdb influx -username admin -password 'secret' \
   -execute 'CREATE USER "pve" WITH PASSWORD '\''STRONG_PASS_HERE'\'''
 
 # grant write (or all) on the proxmox DB
-docker exec -it influxdb influx -username admin -password 'CHANGE_ME' \
+docker exec -it influxdb influx -username admin -password 'secret' \
   -execute 'GRANT ALL ON "proxmox" TO "pve"'
 # If you prefer least-privilege: GRANT WRITE ON "proxmox" TO "pve"
