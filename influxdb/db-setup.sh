33152da140275398c6f8f0c3a501255b4956ff35
M.Gossman <mark.gossman71@gmail.com>
2025-10-31 06:09:07 -0500

    Add Proxmox user setup in db-setup.sh

--- influxdb/db-setup.sh
+++ influxdb/db-setup.sh
@@ -5,3 +5,13 @@ docker exec -it influxdb influx -username admin -password 'CHANGE_ME' \
 # (optional) keep only 30 days by default
 #docker exec -it influxdb influx -username admin -password 'CHANGE_ME' \
   -execute 'CREATE RETENTION POLICY "thirty_days" ON "proxmox" DURATION 30d REPLICATION 1 DEFAULT'
+
+
+# create a Proxmox-specific user
+docker exec -it influxdb influx -username admin -password 'CHANGE_ME' \
+  -execute 'CREATE USER "pve" WITH PASSWORD '\''STRONG_PASS_HERE'\'''
+
+# grant write (or all) on the proxmox DB
+docker exec -it influxdb influx -username admin -password 'CHANGE_ME' \
+  -execute 'GRANT ALL ON "proxmox" TO "pve"'
+# If you prefer least-privilege: GRANT WRITE ON "proxmox" TO "pve"
